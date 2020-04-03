41b1e4c8350c42193c4023732831a98ff1451bfe
邓亮 <dl@cyhzsoft.com>
2020-04-03 15:41:14 +0800

    '所有空页面'

---
+++ src/query/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { connect } from "react-redux";
+
+function App(props) {}
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispath => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
