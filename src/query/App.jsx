e8be4890ed3cef75a8a56791e43243baf2600fe4
邓亮 <dl@cyhzsoft.com>
2020-04-04 17:40:57 +0800

    '查询页面'

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,13 +1,206 @@
-import "./App.css";
-import { connect } from "react-redux";
+import React, { useCallback, useEffect, useMemo } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import URI from 'urijs'
+import dayjs from 'dayjs'
 
-function App(props) {}
-const mapStateToProps = state => {
-  return {};
-};
+import './App.css'
+import Header from '../common/Header'
+import Nav from '../common/Nav'
+import List from './List'
+import Bottom from './Bottom'
 
-const mapDispatchToProps = dispath => {
-  return {};
-};
+import useNav from '../common/useNav'
+import { h0 } from '../common/fp'
+import {
+  setFrom,
+  setTo,
+  setDepartDate,
+  setHighSpeed,
+  setSearchParsed,
+  setTrainList,
+  setTicketTypes,
+  setTrainTypes,
+  setDepartStations,
+  setArriveStations,
+  prevDate,
+  nextDate,
+  toggleOrderType,
+  toggleHighSpeed,
+  toggleOnlyTickets,
+  toggleIsFiltersVisible,
+  setCheckedTicketTypes,
+  setCheckedTrainTypes,
+  setCheckedDepartStations,
+  setCheckedArriveStations,
+  setDepartTimeStart,
+  setDepartTimeEnd,
+  setArriveTimeStart,
+  setArriveTimeEnd,
+} from './actions'
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+function App(props) {
+  const {
+    trainList,
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    orderType,
+    onlyTickets,
+    ticketTypes,
+    trainTypes,
+    departStations,
+    arriveStations,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+    isFiltersVisible,
+    dispatch,
+  } = props
+
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search)
+    const { from, to, highSpeed, date } = queries
+
+    dispatch(setFrom(from))
+    dispatch(setTo(to))
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())))
+    dispatch(setHighSpeed(highSpeed === 'true'))
+
+    dispatch(setSearchParsed(true))
+  }, [dispatch])
+
+  useEffect(() => {
+    if (!searchParsed) {
+      return
+    }
+
+    const url = new URI('/rest/query')
+      .setSearch('from', from)
+      .setSearch('to', to)
+      .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+      .setSearch('highSpeed', highSpeed)
+      .setSearch('orderType', orderType)
+      .setSearch('onlyTickets', onlyTickets)
+      .setSearch('checkedTicketTypes', Object.keys(checkedTicketTypes).join())
+      .setSearch('checkedTrainTypes', Object.keys(checkedTrainTypes).join())
+      .setSearch('checkedDepartStations', Object.keys(checkedDepartStations).join())
+      .setSearch('checkedArriveStations', Object.keys(checkedArriveStations).join())
+      .setSearch('departTimeStart', departTimeStart)
+      .setSearch('departTimeEnd', departTimeEnd)
+      .setSearch('arriveTimeStart', arriveTimeStart)
+      .setSearch('arriveTimeEnd', arriveTimeEnd)
+      .toString()
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        const {
+          dataMap: {
+            directTrainInfo: {
+              trains,
+              filter: { ticketType, trainType, depStation, arrStation },
+            },
+          },
+        } = result
+
+        dispatch(setTrainList(trains))
+        dispatch(setTicketTypes(ticketType))
+        dispatch(setTrainTypes(trainType))
+        dispatch(setDepartStations(depStation))
+        dispatch(setArriveStations(arrStation))
+      })
+  }, [
+    arriveTimeEnd,
+    arriveTimeStart,
+    checkedArriveStations,
+    checkedDepartStations,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    departDate,
+    departTimeEnd,
+    departTimeStart,
+    dispatch,
+    from,
+    highSpeed,
+    onlyTickets,
+    orderType,
+    searchParsed,
+    to,
+  ])
+
+  const onBack = useCallback(() => {
+    window.history.back()
+  }, [])
+
+  const { isPrevDisabled, isNextDisabled, prev, next } = useNav(dispatch, departDate, prevDate, nextDate)
+
+  const bottomCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        toggleOrderType,
+        toggleHighSpeed,
+        toggleOnlyTickets,
+        toggleIsFiltersVisible,
+
+        setCheckedTicketTypes,
+        setCheckedTrainTypes,
+        setCheckedDepartStations,
+        setCheckedArriveStations,
+        setDepartTimeStart,
+        setDepartTimeEnd,
+        setArriveTimeStart,
+        setArriveTimeEnd,
+      },
+      dispatch,
+    )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (!searchParsed) return null
+
+  return (
+    <div>
+      <div className="header-wrapper">
+        <Header title={`${from} ⇀ ${to}`} onBack={onBack} />
+      </div>
+      <Nav date={departDate} isPrevDisabled={isPrevDisabled} isNextDisabled={isNextDisabled} prev={prev} next={next} />
+      <List list={trainList} />
+      <Bottom
+        highSpeed={highSpeed}
+        orderType={orderType}
+        onlyTickets={onlyTickets}
+        isFiltersVisible={isFiltersVisible}
+        ticketTypes={ticketTypes}
+        trainTypes={trainTypes}
+        departStations={departStations}
+        arriveStations={arriveStations}
+        checkedTicketTypes={checkedTicketTypes}
+        checkedTrainTypes={checkedTrainTypes}
+        checkedDepartStations={checkedDepartStations}
+        checkedArriveStations={checkedArriveStations}
+        departTimeStart={departTimeStart}
+        departTimeEnd={departTimeEnd}
+        arriveTimeStart={arriveTimeStart}
+        arriveTimeEnd={arriveTimeEnd}
+        {...bottomCbs}
+      />
+    </div>
+  )
+}
+
+export default connect(
+  function mapStateToProps(state) {
+    return state
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch }
+  },
+)(App)
--- src/query/index.js
+++ src/query/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import 'normalize.css/normalize.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import 'normalize.css/normalize.css'
 
-import store from './store';
-import './index.css';
-import App from './App.jsx';
+import './index.css'
+import App from './App.jsx'
+import store from './store'
 
 ReactDOM.render(
-    <Provider store={store}><App/></Provider>,
-    document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
