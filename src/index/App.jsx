9c8f97c5e42a5e74400019dd4a7d9271fe8012f8
邓亮 <dl@cyhzsoft.com>
2020-04-04 14:16:30 +0800

    '首页完成'

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,13 +1,140 @@
-import "./App.css";
+import React, { useCallback, useMemo } from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import "./App.css";
 
-function App(props) {}
-const mapStateToProps = state => {
-  return {};
-};
+import Header from "../common/Header.jsx";
+import DepartDate from "./DepartDate.jsx";
+import HighSpeed from "./HighSpeed.jsx";
+import Journey from "./Journey.jsx";
+import Submit from "./Submit.jsx";
 
-const mapDispatchToProps = dispath => {
-  return {};
-};
+import CitySelector from "../common/CitySelector.jsx";
+import DateSelector from "../common/DateSelector.jsx";
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+import { h0 } from "../common/fp";
+
+import {
+  exchangeFromTo,
+  showCitySelector,
+  hideCitySelector,
+  fetchCityData,
+  setSelectedCity,
+  showDateSelector,
+  hideDateSelector,
+  setDepartDate,
+  toggleHighSpeed
+} from "./actions";
+
+function App(props) {
+  const {
+    from,
+    to,
+    isCitySelectorVisible,
+    isDateSelectorVisible,
+    cityData,
+    isLoadingCityData,
+    highSpeed,
+    dispatch,
+    departDate
+  } = props;
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector
+      },
+      dispatch
+    );
+  }, [dispatch]);
+
+  const citySelectorCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onBack: hideCitySelector,
+        fetchCityData,
+        onSelect: setSelectedCity
+      },
+      dispatch
+    );
+  }, [dispatch]);
+
+  const departDateCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onClick: showDateSelector
+      },
+      dispatch
+    );
+  }, [dispatch]);
+
+  const dateSelectorCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onBack: hideDateSelector
+      },
+      dispatch
+    );
+  }, [dispatch]);
+
+  const highSpeedCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        toggle: toggleHighSpeed
+      },
+      dispatch
+    );
+  }, [dispatch]);
+
+  const onSelectDate = useCallback(day => {
+    if (!day) {
+      return;
+    }
+
+    if (day < h0()) {
+      return;
+    }
+
+    dispatch(setDepartDate(day));
+    dispatch(hideDateSelector());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={onBack} />
+      </div>
+      <form action="./query.html" className="form">
+        <Journey from={from} to={to} {...cbs} />
+        <DepartDate time={departDate} {...departDateCbs} />
+        <HighSpeed highSpeed={highSpeed} {...highSpeedCbs} />
+        <Submit />
+      </form>
+      <CitySelector
+        show={isCitySelectorVisible}
+        cityData={cityData}
+        isLoading={isLoadingCityData}
+        {...citySelectorCbs}
+      />
+      <DateSelector
+        show={isDateSelectorVisible}
+        {...dateSelectorCbs}
+        onSelect={onSelectDate}
+      />
+    </div>
+  );
+}
+
+export default connect(
+  function mapStateToProps(state) {
+    return state;
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch };
+  }
+)(App);
--- src/index/store.js
+++ src/index/store.js
@@ -1,10 +1,22 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
   combineReducers(reducers),
-  {},
-  applyMiddleware(thunk)
+  {
+    from: "北京",
+    to: "上海",
+    isCitySelectorVisible: false,
+    currentSelectingLeftCity: false,
+    cityData: null,
+    isLoadingCityData: false,
+    isDateSelectorVisible: false,
+    departDate: Date.now(),
+    highSpeed: false
+  },
+  composeEnhancers(applyMiddleware(thunk))
 );
